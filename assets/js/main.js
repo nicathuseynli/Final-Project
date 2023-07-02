24bbc57197552831811c82a708c3d4f21c864136
nicathuseynli <nijatkhh@code.edu.az>
2023-07-02 18:20:42 +0400

    Single product

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,16 @@ function sendimg(a) {
 }
 
 // Single Page end
+//Tabs Start
+const tabs = document.querySelectorAll('.tab_btn');
+const all_content = document.querySelectorAll('.content');
+
+tabs.forEach((tab ,index)=>{
+   tab.addEventListener('click',()=>{
+    tabs.forEach(tab=>{tab.classList.remove('active')});
+    tab.classList.add('active');
+   })
+
+})
+
+//Tabs End
